ac08e03f7e427b4ecf8a63fa9363167e873190d7
Hanan Dev <codewithhanaan@gmail.com>
2025-08-10 08:16:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+/*In this seris I have a PDF question to which I will practicing daily..
+so one i'm complated one task then i comment for my completing more task..
+*/
+//task no-1
+/*
+write a script to greet your website visitor using JS alert box.
+*/
+//alert("Hello Aslamoalikum..")
+
+//task no-2
+/*
+write a code to display message.. the message is (Error!Please enter a valid password)
+*/
+//alert("Error!Please enter a valid pasword.")
+
+//task no-3
+/*
+write a script to display message on your web page : using line brake
+*/
+//alert("Welcome to JS land...\n Happy Coding!")   //\n usnig for line brake only in JS
+
+//task no-4
+/*
+Write a script to display following messages in sequence:
+*/
+//alert("Welcome to JS land..")
+//alert("Happy coding!\n☐Prevent this page from creating additional dialogs. ")
+
+
+//task no-5
+/*
+Generate the any message through browser’s
+developer console:
+*/
+//open browser then right click inspect go console section then 
+//lert("Hello..I can run JS through my web browser's console.")
+
+//task no-6
+/*
+Make use of alerts in your new/existing HTML & CSS
+project.
+
+*/
+//html file for using js code we add <script ><script/>then add alert 
+
+
+//task no -7
+/*
+. Practice placement of <script></script> element in
+following sections of your project in exercise 6:
+a. Head
+b. Body (before your page’s HTML)
+c. Body (inside your page’s HTML)
+d. Body (after your page’s HTML)
+*/
+//all practice done one by one
