65f9b417cd751a7a62b5ba66e17874cdc0ca645f
Hanan Dev <codewithhanaan@gmail.com>
2025-08-11 09:23:49 +0500

    Add files via upload

---
+++ CHAP3/app.js
@@ -0,0 +1,28 @@
+/*let num = prompt("Enter the number")
+if(num>10){
+    console.log("Greater then 10");
+    
+}else{
+    console.log("Less then or Equal to 10");
+    
+}
+*/
+/*let age = +prompt("Enter your age")
+if (age>=18) {
+    console.log("You are adult")
+    
+    
+}else{
+    console.log("You are minor");
+    
+}
+    */
+
+let num = +prompt("Enter number")
+if (num % 2 == 0) {
+    console.log("even");
+    
+}else{
+    console.log("old");
+    
+}
